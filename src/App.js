b6f0b7a236e2e6c9a7beb277ebb0b81e99c78021
senseImamuraY <89624108+senseImamuraY@users.noreply.github.com>
2022-05-05 10:26:12 +0900

    fin ch34

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules//Searchinput";
+import { UserCard } from "./components/organisms/user/UserCard";
+import { HeaderOnly } from "./components/templates/HeaderOnly";
+
 import "./styles.css";
+
+const user = {
+  name: "マンキュー",
+  image: "https://source.unsplash.com/s87ngBxo4xg",
+  email: "12345@example.com",
+  phone: "070-0123-4567",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
-    <div className="App">
+    <HeaderOnly>
       <PrimaryButton>アイスクリーム</PrimaryButton>
       <SecondaryButton>ようかん</SecondaryButton>
-    </div>
+      <br />
+      <SearchInput />
+      <UserCard user={user} />
+    </HeaderOnly>
   );
 }
